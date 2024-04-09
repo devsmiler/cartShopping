281dca7d7d36fd8b3c2ae3805d4016b3fdcbddbc
Ashal aka JOKER <ahastudio@gmail.com>
2024-04-09 20:56:43 +0900

    Add README file and controller test
    Write `README.md` file to run unit tests and web server.
    
    Add basic APIs and their unit tests:
    - Get cart line items.
    - Add a product to cart.

--- src/main/java/com/cart/framework/web/CartController.java
+++ src/main/java/com/cart/framework/web/CartController.java
@@ -1,4 +1,23 @@
 package com.cart.framework.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart/line-items")
 public class CartController {
+    @GetMapping
+    void list() {
+        // TODO: Return DTO containing cart line items.
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    void create() {
+        // TODO: Add product to cart.
+    }
 }
